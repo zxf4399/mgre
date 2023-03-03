e5fdfafe02ffebef5dc105f1355423457a330ca7
zxf4399 <zxf4399@gmail.com>
2023-03-03 09:11:02 +0800

    fix(db): change database file path to .mgre directory closes #29

--- src/utils/constant.mjs
+++ src/utils/constant.mjs
@@ -8,6 +8,8 @@ export const DEFAULT_CONFIG = {
 
 export const CONFIG_PATH = join(DEFAULT_CONFIG.root, DEFAULT_CONFIG.filename)
 
+export const DB_PATH = join(DEFAULT_CONFIG.root, "mgre.db")
+
 export const CONFIG_FIELDS = {
     CODEBASES: "codebases",
 }
