f10cf7c0f2c39fba7d46425e6caf190747798f47
zxf4399 <zxf4399@gmail.com>
2023-03-05 10:34:30 +0800

    refactor(find): make print simplify and clarify
    Closes #30

--- src/commands/find.mjs
+++ src/commands/find.mjs
@@ -1,44 +1,20 @@
-import { cancel, intro, isCancel, note, outro, select } from "@clack/prompts"
-import pc from "picocolors"
+import chalk from "chalk"
 
 import db from "#db"
-import logger from "#logger"
 
 class FindCommand {
     async run(localRepoPath) {
-        try {
-            const repoLocalPaths = await db.get(localRepoPath)
+        const repoLocalPaths = await db.get(localRepoPath)
 
-            intro("Found the localRepoPath in the database.")
-
-            const selectRepoLocalPath = await select({
-                message: "Select a localRepoPath",
-                options: repoLocalPaths.map((item) => ({
-                    label: item.local_repo_path,
-                    value: item.local_repo_path,
-                })),
-            })
-
-            if (isCancel(selectRepoLocalPath)) {
-                logger.debug("User canceled the find prompt operation.")
-
-                cancel('Find canceled by user. Run "mgre find" to try again.')
-
-                process.exit(0)
-            }
-
-            note(`cd ${selectRepoLocalPath}`, "Next steps")
-
-            outro(
-                `Problems? ${pc.underline(
-                    pc.cyan("https://github.com/zxf4399/mgre/issues")
-                )}`
-            )
-        } catch (error) {
-            logger.error(
-                `Can not find the localRepoPath: ${localRepoPath} in the database.`
-            )
+        if (repoLocalPaths.length === 0) {
+            process.exit(0)
         }
+
+        console.log(
+            chalk.cyan(
+                repoLocalPaths.map((item) => item.local_repo_path).join("\n")
+            )
+        )
     }
 }
 
